ea7327b42fad8110c28398aeccd168b0c8045ef3
ADEYEMI NURUDEEN OLATUNBOSUN <adeyemisworld@gmail.com>
2023-01-12 16:18:03 +0100

    Add project files.

---
+++ Batch13OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Batch13OOP;
+
+
+RechargeCard card1 = new RechargeCard("ghgdfg","GLO", 123456, 500.00M, "CLH COMP", DateTime.Now);
+//card1.SetCardType("MTN");
+//card1.SetSerialNumber("asdfgjfkdj345");
+RechargeCard card2 = new RechargeCard("tyuighj", "Etisalat");
+
+Console.WriteLine($"SerialNumber: {card1.GetSerialNumber()}, CardType: {card1.GetCardType()}");
---
+++ Batch13OOP/RechargeCard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch13OOP
+{
+    public class RechargeCard
+    {
+        private string serialNumber;
+        private string cardType;
+        private int cardPin;
+        private decimal cardAmount;
+        private string companyName;
+        private DateTime dateCreated;
+
+        public RechargeCard(string serialNumber, string cardType, int cardPin, decimal cardAmount, string companyName, DateTime dateCreated)
+        {
+            this.serialNumber = serialNumber;
+            this.cardType = cardType;
+            this.cardPin = cardPin;
+            this.cardAmount = cardAmount;
+            this.companyName = companyName;
+            this.dateCreated = dateCreated;
+        }
+
+        public RechargeCard(string serialNumber, string cardType)
+        {
+            this.serialNumber = serialNumber;
+            this.cardType = cardType;
+        }
+
+        public void SetSerialNumber(string serialNumber)
+        {
+            // "this" is a reference keyword
+            this.serialNumber = serialNumber; 
+        }
+
+        public string GetSerialNumber()
+        {
+            return serialNumber;
+        }
+
+        public void SetCardType(string cardType)
+        {
+            // "this" is a reference keyword
+            this.cardType = cardType;
+        }
+
+        public string GetCardType()
+        {
+            return cardType;
+        }
+    }
+}
